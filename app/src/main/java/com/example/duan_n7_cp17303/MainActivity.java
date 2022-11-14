6e1e6faea43ea9d074afb4e9b6f3bbda25c87566
nguyenchamdut <nguyennvph21987@fpt.edu.vn>
2022-11-14 14:50:33 +0700

    Bắt đầu dự án

--- app/src/main/java/com/example/duan_n7_cp17303/MainActivity.java
+++ app/src/main/java/com/example/duan_n7_cp17303/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Nguyên thêm", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Manh them", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "thêm lần 2", Toast.LENGTH_SHORT).show();
     }
 }
