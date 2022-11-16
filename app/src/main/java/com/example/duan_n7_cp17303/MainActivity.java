284484b9a50edbeaac1f1fa25bc38a49995df885
VU THANH LUYEN <luyenvtph24062@gmail.com>
2022-11-16 14:46:49 +0700

    luyen

--- app/src/main/java/com/example/duan_n7_cp17303/MainActivity.java
+++ app/src/main/java/com/example/duan_n7_cp17303/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.duan_n7_cp17303;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.duan_n7_cp17303.Sqlserver.DbSqlServer;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("TAG", "onCreate: ");
 
     }
 }
