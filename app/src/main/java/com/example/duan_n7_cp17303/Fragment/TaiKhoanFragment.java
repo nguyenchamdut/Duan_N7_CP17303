2366d906223f5ee17a3dc71f1cf59b2b5ca9b0e9
nguyenchamdut <nguyennvph21987@fpt.edu.vn>
2022-11-30 10:35:16 +0700

    nguyen

--- app/src/main/java/com/example/duan_n7_cp17303/Fragment/TaiKhoanFragment.java
+++ app/src/main/java/com/example/duan_n7_cp17303/Fragment/TaiKhoanFragment.java
@@ -1,6 +1,9 @@
 package com.example.duan_n7_cp17303.Fragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,10 +14,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.duan_n7_cp17303.Activity.Dangnhap_khach;
+import com.example.duan_n7_cp17303.Activity.HienthittKhach_Activity;
+import com.example.duan_n7_cp17303.Activity.HomeActivity;
 import com.example.duan_n7_cp17303.Activity.Themthongtin;
+import com.example.duan_n7_cp17303.DAO.Daokhachhang;
 import com.example.duan_n7_cp17303.DAO.Daotaikhoan;
+import com.example.duan_n7_cp17303.DTO.Khachhang;
 import com.example.duan_n7_cp17303.DTO.Taikhoan;
 import com.example.duan_n7_cp17303.R;
 
@@ -23,8 +31,10 @@ import java.util.List;
 public class TaiKhoanFragment extends Fragment {
 
     View itemview;
+    Daokhachhang daokhachhang;
+    List<Khachhang> khachhangList;
+    TextView btn_dangnhap, btn_themthongtin, tvtentaikhoan;
 
-    TextView btn_dangnhap, btn_themthongtin;
     public TaiKhoanFragment() {
         // Required empty public constructor
     }
@@ -48,10 +58,25 @@ public class TaiKhoanFragment extends Fragment {
         itemview = inflater.inflate(R.layout.fragment_tai_khoan, container, false);
         btn_dangnhap = itemview.findViewById(R.id.ll_dangnhap);
         btn_themthongtin = itemview.findViewById(R.id.ll_thongtin);
+        tvtentaikhoan = itemview.findViewById(R.id.tv_tentaikhoan);
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("Login", MODE_PRIVATE);
+        String u = sharedPreferences.getString("name", "");
+        String p = sharedPreferences.getString("pass", "");
+
+        if (u.equals("") || p.equals("")){
+            btn_dangnhap.setText("Đăng nhập");
+        }else {
+            btn_dangnhap.setText("Đăng Xuất");
+            tvtentaikhoan.setText(u);
+        }
+
+        btn_themthongtin.setText("Thông tin khách hang");
 
         btn_dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                dangxuat();
                 Intent intent = new Intent(getContext(), Dangnhap_khach.class);
                 startActivity(intent);
             }
@@ -60,11 +85,89 @@ public class TaiKhoanFragment extends Fragment {
         btn_themthongtin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(getContext(), Themthongtin.class);
-                startActivity(intent1);
+                ttkhachhang();
             }
         });
 
         return itemview;
     }
+
+    void dangxuat(){
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("Login", MODE_PRIVATE);
+        String u = sharedPreferences.getString("name", "");
+        String p = sharedPreferences.getString("pass", "");
+
+        if (u.equals("") || p.equals("")){
+            Log.d("TAG", "dangnhap");
+            Intent intent = new Intent(getContext(), Dangnhap_khach.class);
+            startActivity(intent);
+        }else {
+            Log.d("TAG", "dang xuat");
+            Toast.makeText(getContext(), "Bạn đã đăng xuất thành công", Toast.LENGTH_SHORT).show();
+            SharedPreferences preferences = getContext().getSharedPreferences("Login", MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.clear();
+            editor.commit();
+            Intent intent = new Intent(getContext(), HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    void ttkhachhang(){
+        daokhachhang = new Daokhachhang();
+        khachhangList = daokhachhang.getAll();
+        Log.d("n", "ttkhachhang: " + khachhangList);
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("Login", MODE_PRIVATE);
+        String u = sharedPreferences.getString("name", "");
+
+        if (u.equals("")){
+            //Chưa đăng nhập cần đăng nhập
+            Toast.makeText(getContext(), "Bạn cần phải đăng nhập", Toast.LENGTH_SHORT).show();
+        } else if (!u.equals("")) {
+            for (int i = 0; i < khachhangList.size(); i++) {
+                Khachhang khachhang = khachhangList.get(i);
+                Log.d("cc", "ttkhachhang: " + khachhang.getUsername() + " ," + khachhang.getId_khachhang());
+                if (khachhang.getUsername().equals(u)) {
+                    Log.d("cc", "Đã chạy vào if này: ");
+
+                    SharedPreferences pref = getContext().getSharedPreferences("MyPref", MODE_PRIVATE); // 0 - for private mode
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("key_name", khachhang.getUsername());
+                    editor.putInt("key_id", khachhang.getId_khachhang());
+                    editor.putString("key_hoten", khachhang.getHoten());
+                    editor.putString("key_sodienthoai", String.valueOf(khachhang.getDienthoai()));
+                    editor.putString("key_email", khachhang.getEmail());
+                    editor.putString("key_diachi", khachhang.getDiachi());
+                    editor.commit();
+
+                }
+            }
+
+            SharedPreferences gg = getContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+            String usertt = gg.getString("key_name", "");
+            String hoten = gg.getString("key_hoten", "");
+            String sodienthoai = gg.getString("key_sodienthoai", "");
+            String email = gg.getString("key_email", "");
+            String diachi = gg.getString("key_diachi", "");
+
+            Log.d("cc", "Đang gọi username khach hang:  " + usertt);
+            if (usertt.equals(u)) {
+                //Hiển thị thông tin khách hàng
+                Intent intent = new Intent(getContext(), HienthittKhach_Activity.class);
+                intent.putExtra("key_hoten", hoten);
+                intent.putExtra("key_sodienthoai", sodienthoai);
+                intent.putExtra("key_email", email);
+                intent.putExtra("key_diachi", diachi);
+                startActivity(intent);
+                Log.d("cc", "Đã hiển thị thông tin khách hàng: ");
+            } else {
+                //thêm thông tin khách hàng
+                Log.d("cc", "Cần thêm thông tin khách hàng: ");
+                Intent intent1 = new Intent(getContext(), Themthongtin.class);
+                startActivity(intent1);
+            }
+
+        }
+}
 }
